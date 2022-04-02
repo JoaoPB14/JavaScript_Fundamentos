aec46b4be051283308094fa0cca681c1578e5007
João Bento <56631964+JoaoPB14@users.noreply.github.com>
2022-04-02 17:02:58 -0300

    Anotações das primeiras aulas do curso

---
+++ aula02-strings-e-numbers.js
@@ -0,0 +1,24 @@
+let salarioDoAmigo = 1000
+let meuSalario = "2999"
+let meuSalarioCorrigido = Number(meuSalario)
+
+console.log(salarioDoAmigo + meuSalarioCorrigido)
+
+console.log(
+    typeof(salarioDoAmigo),
+    salarioDoAmigo
+)
+console.log(
+    typeof(meuSalario),
+    meuSalario
+)
+console.log(
+    isNaN(meuSalario)
+)
+
+let minhaString = 'Olá mundo!'
+let minhaOutraString = 'Olá mundo2'
+let minhaStringComVariaveis = 
+`${minhaString} - ${minhaOutraString}. SALVE!`
+
+console.log(minhaStringComVariaveis)
